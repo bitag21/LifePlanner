ce7b68936e281374328291c6afeddc48778bce3a
Wuberst <wuberstbizengaw@gmail.com>
2026-09-24 22:00:47 +0300

    Initial LifePlanner dashboard

---
+++ src/main/java/com/lifeplanner/lifeplanner/LifeplannerApplication.java
@@ -0,0 +1,13 @@
+package com.lifeplanner.lifeplanner;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LifeplannerApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LifeplannerApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/lifeplanner/lifeplanner/LifeplannerApplicationTests.java
@@ -0,0 +1,13 @@
+package com.lifeplanner.lifeplanner;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LifeplannerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
